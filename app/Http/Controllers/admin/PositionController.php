d59f180c18dbbac62584305d15d6c27554c1ee6f
atris314 <farinaz.haghighi314@gmail.com>
2025-10-11 16:42:56 +0330

    position admin controller check

--- app/Http/Controllers/admin/PositionController.php
+++ app/Http/Controllers/admin/PositionController.php
@@ -36,7 +36,7 @@ class PositionController extends Controller
             $position->title = $request->title;
             $position->save();
 
-            return redirect(route('positions.index'))->with('flash_message', 'با موفقیت ایجاد شد');
+            return redirect(route('admin.positions.index'))->with('flash_message', 'با موفقیت ایجاد شد');
         } catch (Exception $exception) {
             return redirect()->back()->with('err_message', $exception->getMessage());
         }
@@ -67,7 +67,7 @@ class PositionController extends Controller
             $position->title = $request->title;
             $position->update();
 
-            return redirect(route('positions.index'))->with('flash_message', 'با موفقیت ویرایش شد');
+            return redirect(route('admin.positions.index'))->with('flash_message', 'با موفقیت ویرایش شد');
         } catch (Exception $exception) {
             return redirect()->back()->with('err_message', $exception->getMessage());
         }
@@ -80,7 +80,7 @@ class PositionController extends Controller
     {
         try {
             $position->delete();
-            return redirect(route('positions.index'))->with('flash_message', ' موفقیت حذف شد');
+            return redirect(route('admin.positions.index'))->with('flash_message', ' موفقیت حذف شد');
         } catch (Exception $exception) {
             return redirect()->back()->with('err_message', 'خطایی رخ داد مجددا تلاش کنید');
         }
