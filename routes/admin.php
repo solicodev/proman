6aee420b36b4b8d2bd5324e61204c2e95e4a9a02
atris314 <farinaz.haghighi314@gmail.com>
2025-10-11 16:33:04 +0330

    position route

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,18 @@
 <?php
 
 use App\Http\Controllers\admin\adminController;
+use App\Http\Controllers\admin\PositionController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('panel', [adminController::class , 'index'])->name('index');
+Route::prefix('panel')->name('admin.')->group(function () {
+    Route::get('admin', [adminController::class , 'index'])->name('index');
+
+    Route::prefix('position')->name('position.')->group(function () {
+        Route::get('/', [PositionController::class , 'index'])->name('index');
+        Route::get('/create', [PositionController::class , 'create'])->name('create');
+        Route::post('/store', [PositionController::class , 'store'])->name('store');
+        Route::get('/edit/{position}', [PositionController::class , 'edit'])->name('edit');
+        Route::put('/update/{position}', [PositionController::class , 'update'])->name('update');
+        Route::get('/delete/{position}', [PositionController::class , 'destroy'])->name('destroy');
+    });
 });
